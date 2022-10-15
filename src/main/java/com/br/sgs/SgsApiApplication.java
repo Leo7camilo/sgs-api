1863262d70ec0929b9991f082edfeda38eca00af
Leonardo Cardoso Camilo <leolive_17@hotmail.com>
2022-10-14 23:25:34 -0300

    Start application
    Initial Commit

---
+++ src/main/java/com/br/sgs/SgsApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.sgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsApiApplication.class, args);
+	}
+
+}
